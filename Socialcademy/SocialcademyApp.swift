7696313a569ff5542fb0b5f293bfe6e25d9157f0
highness-ru <highness.ru@gmail.com>
2026-03-31 23:59:29 +0100

    Initial Commit

---
+++ Socialcademy/SocialcademyApp.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+import Firebase
+
+@main
+struct SocialcademyApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    var body: some Scene {
+        WindowGroup {
+                PostsList()
+            }
+        }
+    }
+
---
+++ Socialcademy/View Models/PostsViewModel.swift
@@ -0,0 +1,14 @@
+import Foundation
+internal import Combine
+
+@MainActor
+class PostsViewModel: ObservableObject {
+    @Published var posts = [Post.testPost]
+    
+    func makeCreateAction() -> NewPostForm.CreateAction {
+        return { [weak self] post in
+            try await PostsRepository.create(post)
+            self?.posts.insert(post, at: 0)
+        }
+    }
+}
